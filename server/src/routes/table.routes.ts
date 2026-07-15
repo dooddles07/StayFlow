c27c22142d58d6a4cb248ea2d817d9ea7c5def0a
dooddles07 <brixdodd07@gmail.com>
2026-07-15 17:22:42 +0800

    Revert "feat(server): add Express + Prisma MVC backend with JWT auth for all StayFlow resources"
    This reverts commit c336419e2eb61dba2039089c3d11c899faca539a.

--- server/src/routes/table.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import * as TableController from '../controllers/table.controller.js'
-import { asyncHandler } from '../utils/asyncHandler.js'
-import { requireAuth } from '../middleware/auth.middleware.js'
-
-export const tableRouter = Router()
-
-tableRouter.use(requireAuth)
-tableRouter.get('/', asyncHandler(TableController.list))
